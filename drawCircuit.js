806e453f20c57178e646c55ee870df06c06d7392
Daniel Waters <danwater1@gmail.com>
2019-04-14 12:54:45 -0700

    Adds an OR gate

--- drawCircuit.js
+++ drawCircuit.js
@@ -19,7 +19,21 @@ class CircuitItem {
   draw(ctx) {}
 }
 
-class ORGate extends CircuitItem {}
+class ORGate extends CircuitItem {
+  constructor(x, y, value) {
+    super(x, y, 50, 50);
+    this.value = value;
+  }
+
+  draw(ctx) {
+    ctx.strokeStyle = "#000";
+    ctx.font = "36px serif";
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.x + this.width, this.y + this.height);
+    ctx.stroke();
+  }
+}
 
 class Input extends CircuitItem {
   constructor(x, y, value) {
