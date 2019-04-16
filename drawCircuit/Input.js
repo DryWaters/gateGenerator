8811953978051bf38b640a15a5c9c023b595cebd
Daniel Waters <danwater1@gmail.com>
2019-04-15 19:21:28 -0700

    Overrides randomness on inputs

--- drawCircuit/Input.js
+++ drawCircuit/Input.js
@@ -3,10 +3,9 @@ import CircuitItem from "./CircuitItem.js";
 export default class Input extends CircuitItem {
   constructor(x, y, name, value) {
     super(x, y, 50, 50, name, value);
-
-    // override randomness for inputs
     this.x = x;
     this.y = y;
+
     this.outputLocation = {
       x: this.x + this.width,
       y: this.y + this.height / 2
@@ -14,7 +13,7 @@ export default class Input extends CircuitItem {
   }
 
   draw(value, ctx) {
-    // Draw input 
+    // Draw input
     ctx.beginPath();
     ctx.strokeStyle = "#000";
     ctx.strokeRect(this.x, this.y, this.width, this.height);
@@ -29,7 +28,13 @@ export default class Input extends CircuitItem {
     // Draw connector endpoints
     ctx.fillStyle = "red";
     ctx.beginPath();
-    ctx.arc(this.outputLocation.x, this.outputLocation.y, this.CONNECTOR_SIZE, 0, 2 * Math.PI);
+    ctx.arc(
+      this.outputLocation.x,
+      this.outputLocation.y,
+      this.CONNECTOR_SIZE,
+      0,
+      2 * Math.PI
+    );
     ctx.fill();
   }
 }
